29788611c5d1f658c07460f52fa74d1c81ca5068
joanta-tudor <62553012+joanta-tudor@users.noreply.github.com>
2022-10-19 23:32:58 +0300

    Add files via upload

---
+++ MouseMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mousewrld = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, transform.position.y));
+        transform.LookAt(mousewrld);
+        //transform.Rotate(Vector3.up,0f);
+    }
+}
---
+++ MoveIt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public class MoveIt : MonoBehaviour
+{
+    public int minDegrees = 30;
+    public int maxDegrees = 30;
+
+    public float rotationSpeed = 20f;
+
+    public Vector3 rotationAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //update is called once per frame
+    void Update()
+    {
+            
+        float angle = Mathf.PingPong(Time.time * rotationSpeed, minDegrees + maxDegrees) - minDegrees;
+
+        transform.localRotation = Quaternion.AngleAxis(angle, rotationAxis);
+    }
+}
